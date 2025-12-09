6a2c2cefa56440abdca157179a537e08c48e960a
Smile BS <martin.lardiere1@gmail.com>
2025-12-09 16:37:51 +0100

    Implement spiral.py with function definition

--- spiral.py
+++ spiral.py
@@ -2,5 +2,16 @@ from turtle import *
 from random import *
 from math import *
 
-def spiral():
-  pass
+def spiral(nb_circles, thickness, size):
+    hideturtle()
+    setheading(0)
+    speed(10)
+    penup()
+    goto(0, 0)
+    pendown()
+    pensize(thickness)
+    nb_actions = 360 * nb_circles
+    for i in range(nb_actions):
+        color(randint(0, 255), randint(0, 255), randint(0, 255))
+        forward(i / size)
+        left(1)
